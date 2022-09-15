051753a29368b5d392922035a4fc96e4814289c9
DAF201 <1641853573@qq.com>
2022-09-15 00:01:20 -0500

    Update string++.h

--- client/src/libs/string++.h
+++ client/src/libs/string++.h
@@ -8,6 +8,16 @@ using namespace std;
 
 // for all string thing I may need in the future
 
+/*
+definitely, I can make something like
+    
+    void str_to_vec(string input_str,str_vec destination){...}
+
+but I just don't want... I don't want to change my code style because I need to temporary use c++ while most of the time I was using script languages like lua js py
+
+Network programming... I assume the network will not be faster than CPU with in 20 years (Now the network is already faster than my USB port which is crazy...)
+
+*/
 typedef vector<string> str_vec;
 
 str_vec str_split_by_space(string input)
@@ -29,6 +39,16 @@ str_vec str_split_by_space(string input)
     }
     result.push_back(temp);
     return result;
+}
+
+string strvec_to_str(str_vec input_vec)
+{
+    string temp = "";
+    for (int i = 0; i < input_vec.size(); i++)
+    {
+        temp = temp + input_vec[i];
+    }
+    return temp;
 }
 
 // int is sub
