9b6dc21921f7b3bcc86e8d22102e85d7a6d8b632
DAF201 <1641853573@qq.com>
2022-09-14 20:01:58 -0500

    update tools

--- client/src/libs/b64++.hpp
+++ client/src/libs/b64++.hpp
@@ -12,7 +12,17 @@ using namespace std;
 
 typedef char byte_data;
 
-vector<byte_data> read_binary_file(const char *filename);
+vector<byte_data> read_binary_file(const char *filename)
+{
+    streampos fileSize;
+    ifstream file(filename, ios::binary);
+    file.seekg(0, ios::end);
+    fileSize = file.tellg();
+    file.seekg(0, ios::beg);
+    vector<byte_data> fileData(fileSize);
+    file.read((char *)&fileData[0], fileSize);
+    return fileData;
+}
 
 template <typename T>
 string b64encode(T &source)
--- client/src/libs/string++.h
+++ client/src/libs/string++.h
@@ -2,11 +2,54 @@
 #include <string>
 #include <cstring>
 #include <typeinfo>
+#include <type_traits>
+
 using namespace std;
 
 // for all string thing I may need in the future
 
 typedef vector<string> str_vec;
 
-str_vec str_split_by_space(string input);
+str_vec str_split_by_space(string input)
+{
+    string temp = "";
+    str_vec result;
+    for (int i = 0; i < input.length(); ++i)
+    {
 
+        if (input[i] == ' ')
+        {
+            result.push_back(temp);
+            temp = "";
+        }
+        else
+        {
+            temp.push_back(input[i]);
+        }
+    }
+    result.push_back(temp);
+    return result;
+}
+
+template <typename T>
+bool is_sub(T &main_source, T &sub_source)
+{
+    if (is_same<T, int>::value)
+    {
+        string main_source_str = to_string(main_source);
+        string sub_source_str = to_string(sub_source);
+
+        if (0 == strcmp(main_source_str.c_str(), sub_source_str.c_str()))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    if (is_same<T, string>::value)
+    {
+    }
+}
