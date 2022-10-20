d3586974bddbc1f0dbf31ee749291c11cc6fa37a
DAF201 <1641853573@qq.com>
2022-10-19 19:30:32 -0500

    Update config.py

--- server/src/config.py
+++ server/src/config.py
@@ -6,12 +6,10 @@ config = {
 
 HEADER = b'bloom-in protocol V\d\.\d.\d <channel>\w{0,16}<channel> <id>\w{0,16}<id> BLOOM_IN$'
 
-COMMAND = b'bloom-in \w <no>\w{0,256}<no><channel>\w{0,16}<channel><id>\w{0,16}<id><target>\w{0,16}<target><data>(?:[A-Za-z0-9+\/]{4})*(?:[A-Za-z0-9+\/]{4}|[A-Za-z0-9+\/]{3}=|[A-Za-z0-9+\/]{2}={2})<data>BLOOM_IN$'
+COMMAND = b'bloom-in \w <no>(\w{1,2}|-1)<no><channel>\w{0,16}<channel><id>\w{0,16}<id><target>\w{0,16}<target><data>(?:[A-Za-z0-9+\/]{4})*(?:[A-Za-z0-9+\/]{4}|[A-Za-z0-9+\/]{3}=|[A-Za-z0-9+\/]{2}={2})<data>BLOOM_IN$'
 
 EXIT = b'bloom-in exit V\d\.\d.\d <exit><id>\w{0,16}<id><exit> BLOOM_IN$'
 
 COMMAND_POOL = []
 
 EXTRACTOR_SYNTAX = (b'<target>', b'<data>')
-
-SENDER_SYNTAX = b''
