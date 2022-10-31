01ad3ff887deb06ae1c9be1b7b1317a5c721a6e7
DAF201 <daf201@blink-in.com>
2022-10-31 13:43:30 -0500

    update

--- client/src/libs/curl.h
+++ client/src/libs/curl.h
@@ -4,5 +4,9 @@
 #include <file++.h>
 #include <iostream>
 using namespace std;
+// upload
+// curl.exe -X POST 'http://50.17.163.77:5232/upload?file_name=test.txt&file_size=1000' -F 'file=@test.txt'
 
+// download
+// curl.exe -o C:\Users\daf20\Downloads\test.txt 'http://50.17.163.77:5232/download?file_name=test.txt'
 #endif
