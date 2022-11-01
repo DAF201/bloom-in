b33c9e1da33161282124cd5dd0769cd3389bf874
DAF201 <daf201@blink-in.com>
2022-11-01 15:36:16 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,6 +2,24 @@
 
 #add this to home, and execute it, logout
 
-nohup python3 bloom-in/bloom-in.py
+# nohup python3 bloom-in/bloom-in.py
 
-nohup python3 bloom-in/file.py
+# nohup python3 bloom-in/file.py
+
+while true; do
+
+    for pid in $(ps -ef | grep python3 | awk '{print $2}'); do
+
+        kill -9 $pid
+
+    done
+
+    cd ~
+
+    nohup python3 bloom-in/bloom-in.py
+
+    nohup python3 bloom-in/file.py
+
+    sleep 15m
+
+done
